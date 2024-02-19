c7031d134a0e4a1d44e8607f7c3f6ba93e2f0b29
Tommy Boi <tomaarongrabowski@gmail.com>
2024-02-19 05:15:29 -0500

    Testing Z-Index of navbar on Safari mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavigationIcon from '../NavigationIcon/NavigationIcon'
 
 export default function Navbar() {
   return (
-    <aside className="sidebar absolute bottom-0 left-0 right-0 flex h-16 w-full items-center justify-around overflow-hidden bg-mint-cream md:static md:h-screen md:w-20 md:flex-col md:justify-normal md:gap-4 md:shadow-xl">
+    <aside className="sidebar absolute bottom-0 left-0 right-0 z-10 flex h-16 w-full items-center justify-around overflow-hidden bg-mint-cream md:static md:h-screen md:w-20 md:flex-col md:justify-normal md:gap-4 md:shadow-xl">
       <div className="hidden h-16 w-full items-center justify-center bg-celestial-blue md:flex">
         <h2 className="sidebar-icon text-lg text-mint-cream">TG</h2>
       </div>
